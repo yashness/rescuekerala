a9101afda1bf03863c64283df04e0e6617faa1b0
Biswas B <Biswasb007@gmail.com>
2018-08-16 01:59:17 +0530

    Install Raven

--- floodrelief/settings.py
+++ floodrelief/settings.py
@@ -47,7 +47,7 @@ RAVEN_CONFIG = {
     'dsn': env('SENTRY_DSN'),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
+    # 'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
 }
 
 
