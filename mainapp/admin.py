afe7e954ce2b143bb4940a5e020527d8fbbe41a7
Vignesh Hari <vichuhari100@gmail.com>
2018-08-16 00:09:00 +0530

    Requests to csv maker Done

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from .models import Request, Volunteer, Contributor, DistrictNeed, DistrictCollection, DistrictManager
-
+import csv
+from django.http import HttpResponse
 
 class RequestAdmin(admin.ModelAdmin):
+    actions = ['download_csv']
     readonly_fields = ('dateadded',)
     ordering = ('district',)
+    def download_csv(self, request, queryset):
+        f = open('test.csv', 'w')
+        writer = csv.writer(f)
+        l = []
+        for i in (Request._meta.get_fields()):
+            l.append(i.name)
+        writer.writerow(l)
+        data = Request.objects.all().values_list()
+        for s in data:
+            writer.writerow(s)
+        f.close()
+        f = open('test.csv', 'r')
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=requests.csv'
+        return response
+
     list_filter = ('district', 'status',)
 
 
