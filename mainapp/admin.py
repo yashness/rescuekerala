d818337b9ab1676f4040e96d0e3af61cc201029b
Vignesh Hari <vichuhari100@gmail.com>
2018-08-16 16:24:24 +0530

    Added Volunteer CSV , and backend for map

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -20,16 +20,33 @@ class RequestAdmin(admin.ModelAdmin):
         f.close()
         f = open('test.csv', 'r')
         response = HttpResponse(f, content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename=requests.csv'
+        response['Content-Disposition'] = 'attachment; filename=Requests.csv'
         return response
 
     list_filter = ('district', 'status',)
 
 
 class VolunteerAdmin(admin.ModelAdmin):
+    actions = ['download_csv']
     readonly_fields = ('joined',)
     list_display = ('name', 'phone', 'organisation', 'joined')
     list_filter = ('district', 'joined',)
+    def download_csv(self, request, queryset):
+        f = open('test.csv', 'w')
+        writer = csv.writer(f)
+        l = []
+        for i in (Volunteer._meta.get_fields()):
+            l.append(i.name)
+        writer.writerow(l)
+        data = Volunteer.objects.all().values_list()
+        for s in data:
+            writer.writerow(s)
+        f.close()
+        f = open('test.csv', 'r')
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=Volunteers.csv'
+        return response
+
 
 class ContributorAdmin(admin.ModelAdmin):
     list_filter = ('district', 'status',)
